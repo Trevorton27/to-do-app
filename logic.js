1b8b493dab50199d5c0e0b656eaf07648248c0d0
Trevor Mearns <spiral272@gmail.com>
2019-12-18 12:22:10 +0900

    stuck on how to work with local storage

--- logic.js
+++ logic.js
@@ -18,20 +18,20 @@ form.addEventListener('submit', event => {
 });
 
 const list = document.querySelector('.js-todo-list');
-let todoItem;
+
 function addTodo(text) {
+ 
     const todo = {
       text,
       checked: false,
       id: Date.now(),
-
     };
-  
+
     todoItems.push(todo);
       console.log(todo);  
       const theList = JSON.stringify(todoItems);
     localStorage.setItem('todoItem', theList);
- 
+    console.log('This is the list: ', theList);
    
     list.insertAdjacentHTML('beforeend', `
       <li class="todo-item" data-key="${todo.id}">
@@ -44,8 +44,28 @@ function addTodo(text) {
       </li>
     `)};
   
+document.onload = function() {
+  if(localStorage.getItem('todoItem') !== null) {
+    theList = JSON.parse(localStorage.getItem('todoItem'));
+  } 
 
-  
+  console.log(theList);
+  theList.push('todoItem');
+
+  theList = list;
+
+  list.insertAdjacentHTML('beforeend', `
+      <li class="todo-item" data-key="${todo.id}">
+        <input id="${todoItem.id}" type="checkbox"/>
+        <label for="${todoItem.id}" class="tick js-tick"></label>
+        <span>${todoItem.text}</span>
+        <button class="delete-todo js-delete-todo">
+          <svg><use href="#delete-icon"></use></svg>
+        </button>
+      </li>
+    `);
+};
+
 list.addEventListener('click', event => {
   if (event.target.classList.contains('js-tick')) {
     const itemKey = event.target.parentElement.dataset.key;
@@ -68,25 +88,23 @@ function toggleDone(key) {
     } else {
       item.classList.remove('done');
     }
-
-    
   };
   
   function deleteTodo(key) {
     todoItems = todoItems.filter(item => item.id !== Number(key));
     const item = document.querySelector(`[data-key='${key}']`);
-    //alert('You sure about that, now?')
+    //alert('You sure about that, now?');
     item.remove();
     const retrieve = localStorage.getItem('todoItem');
     const stringToArray = retrieve.split();
     const findIt = stringToArray.indexOf(item);
 
     item.delete(findIt);
+    localStorage.removeItem(findIt);
     
     let backToString = JSON.stringify(stringToArray);
 
     console.log('backToString: ', backToString);
-
       };
 
     

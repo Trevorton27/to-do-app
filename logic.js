17667a81eb2b08ad2fab7f17401bce4ce4c5063c
Trevor Mearns <spiral272@gmail.com>
2019-12-20 15:06:08 +0900

    still stuck as a duck

--- logic.js
+++ logic.js
@@ -1,4 +1,4 @@
-let todoItems = JSON.parse(localStorage.getItem('todoItem')) || [];
+let todoItems = [];
 
 const form = document.querySelector('.js-form');
 form.addEventListener('submit', event => {
@@ -14,47 +14,43 @@ form.addEventListener('submit', event => {
   
 });
 
-const list = document.querySelector('.js-todo-list');
+const renderTodos = document.getElementById('todoLi');
+renderTodos.addEventListener("load", pullTodosFromLocal, console.log('pullTodosFromLocal has fired'));
+
+
+const list = document.getElementById('js-todo-list');
 
 function addTodo(text) {
  
-    const todo = {
-      text,
-      checked: false,
-      id: Date.now(),
-    };
-
+  const todo = {
+    text,
+    checked: false,
+    id: Date.now(),
+  };
     todoItems.push(todo);
       console.log(todo);  
       
     localStorage.setItem('todoItem', JSON.stringify(todoItems));
     console.log('This is the list: ', JSON.stringify(todoItems));
-   
+    
     list.insertAdjacentHTML('beforeend', `
-      <li class="todo-item" id="todoLi" data-key="${todo.id}">
-        <input id="${todo.id}" type="checkbox"/>
-        <label for="${todo.id}" class="tick js-tick"></label>
-        <span>${todo.text}</span>
-        <button class="delete-todo js-delete-todo">
-          <svg><use href="#delete-icon"></use></svg>
-        </button>
-      </li>
-    `)};
-  
- /*function checkLocalStorage() {
-  if(localStorage.getItem('todoItem') !== null) {
-    JSON.parse(localStorage.getItem('todoItem'));
-  } 
+  <li class="todo-item" id="todoLi" data-key="${todo.id}">
+    <input id="${todo.id}" type="checkbox"/>
+    <label for="${todo.id}" class="tick js-tick"></label>
+    <span>${todo.text}</span>
+    <button class="delete-todo js-delete-todo">
+      <svg><use href="#delete-icon"></use></svg>
+    </button>
+  </li>
+`)};
 
-  
-  todoItems.push('todoItem');
- 
-  addTodo();
-  console.log('am i working');
+function pullTodosFromLocal() {
+ const gotIt = JSON.parse(localStorage.getItem('todoItem')); 
+ console.log(gotIt);
+}
 
-};*/
+    
 
-//document.querySelector('.js-todo-list').addEventListener("load", checkLocalStorage);
 
 list.addEventListener('click', event => {
   if (event.target.classList.contains('js-tick')) {
@@ -95,7 +91,7 @@ function toggleDone(key) {
     localStorage.removeItem('todoItem');
 
     let backToString = JSON.stringify(stringToArray);
-    localStorage.removeItem(backToString);
+    //localStorage.removeItem(backToString);
 
     console.log('backToString: ', backToString);
       };
